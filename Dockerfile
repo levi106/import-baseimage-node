efd63d7c670183b3a29d2f8fc62d80c832b058b6
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2022-12-09 16:08:54 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 ARG REGISTRY_FROM_URL=
-FROM ${REGISTRY_FROM_URL}node:15-alpine
+FROM ${REGISTRY_FROM_URL}helloworld:test
 WORKDIR /test
 COPY ./test.sh .
 CMD ./test.sh
--- test.sh
+++ test.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-echo -e "\e[32mValidating Image\e[0m"
 
-echo NODE_VERSION: ${NODE_VERSION}
-echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
-
-if [ ""$(echo $BACKGROUND_COLOR | tr '[:lower:]' '[:upper:]') = 'RED' ]; then
-    echo -e "\e[31mERROR: Invalid Color:\e[0m" ${BACKGROUND_COLOR}
-    EXIT_CODE=1
-else
-  echo -e "\e[32mValidation Complete - No Known Errors\e[0m"
-fi
-
-exit ${EXIT_CODE}
+exit 0
